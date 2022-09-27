80ebd787cc562ac855d581fc67cd88e0e95ba96b
Nicolas <nicolascari.rz@gmail.com>
2022-09-26 22:57:48 -0400

    cambiando ejecutarse por moverse en sumador.test

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -8,7 +8,7 @@ describe("mover auto en grid", () => {
   it("deberia mostrar posicion inicial 0,0N", () => {
     expect(auto.ejecutar("")).toEqual("0,0N");
   });
-  it("deberia ejecutarse hacia el norte", () => {
+  it("deberia moverse hacia el norte", () => {
     expect(auto.ejecutar("A")).toEqual("0,1N");
   });
   it("deberia cambiar de direccion a la derecha", () => {
@@ -17,25 +17,25 @@ describe("mover auto en grid", () => {
   it("deberia cambiar de direccion a la izquierda", () => {
     expect(auto.ejecutar("I")).toEqual("0,0O");
   });
-  it("deberia de ejecutarse muchos pasos hacia el norte", () => {
+  it("deberia de moverse muchos pasos hacia el norte", () => {
     expect(auto.ejecutar("AAA")).toEqual("0,3N");
   });
-  it("deberia de ejecutarse muchos pasos hacia el este", () => {
+  it("deberia de moverse muchos pasos hacia el este", () => {
     expect(auto.ejecutar("ADAA")).toEqual("2,1E");
   });
-  it("deberia de ejecutarse muchos pasos hacia el oeste", () => {
+  it("deberia de moverse muchos pasos hacia el oeste", () => {
     expect(auto.ejecutar("ADAAIIAA")).toEqual("0,1O");
   });
-  it("deberia de ejecutarse muchos pasos hacia el sur", () => {
+  it("deberia de moverse muchos pasos hacia el sur", () => {
     expect(auto.ejecutar("AAAAADDAAA")).toEqual("0,2S");
   });
-  it("deberia de ejecutarse muchos pasos hacia el sur por distinto camino", () => {
+  it("deberia de moverse muchos pasos hacia el sur por distinto camino", () => {
     expect(auto.ejecutar("AAAAAIIAAA")).toEqual("0,2S");
   });
-  it("deberia de ejecutarse por todo el mapa sin salir de los bordes, esta en bordes", () => {
+  it("deberia de moverse por todo el mapa sin salir de los bordes, esta en bordes", () => {
     expect(auto.ejecutar("ADAAIADADAA")).toEqual("3,0S");
   });
-  it("deberia de ejecutarse por todo el mapa sin salir de los bordes,no esta en bordes", () => {
+  it("deberia de moverse por todo el mapa sin salir de los bordes,no esta en bordes", () => {
     expect(auto.ejecutar("AIIAAA")).toEqual("se salio de bordes");
   });
 });
